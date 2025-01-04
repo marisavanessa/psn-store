055a50bfb920103aa13b513270f1652747ded43e
Marisa Vanessa N. Cruz <ideiaselembrancas@gmail.com>
2025-01-04 19:57:20 -0300

    Item 11 Criando componentes a nível de átomo

--- store/src/app/app.module.ts
+++ store/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { MenubarComponent } from './components/menubar/menubar.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPricingComponent } from './components/card/card-pricing/card-pricing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CardComponent,
-    MenubarComponent
+    MenubarComponent,
+    CardLabelComponent,
+    CardPricingComponent
   ],
   imports: [
     BrowserModule,
